bc6bbc1b911092d1297842627eb17ee1567d0a81
Nikolay Ivanov <nick.ivanov98@gmail.com>
2024-11-23 08:24:24 +0200

    Adding simple events

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -5,6 +5,7 @@ pragma solidity 0.8.19;
  * Errors
  */
 error Raffle__NotEnoughETH();
+error Raffle__NotEnoughTimeHasPassed();
 
 /**
  * @title Raffle
@@ -14,16 +15,43 @@ error Raffle__NotEnoughETH();
  */
 contract Raffle {
     uint256 private immutable i_entranceFee;
+    /**
+     * @dev The interval between raffles in seconds
+     */
+    uint256 private immutable i_interval;
 
-    constructor(uint256 _entranceFee) {
+    address payable[] private s_players;
+    uint256 private s_lastTimestamp;
+
+    /* Events */
+    event RaffleEnter(address indexed player);
+
+    constructor(uint256 _entranceFee, uint256 interval) {
         i_entranceFee = _entranceFee;
+        i_interval = interval;
+
+        s_lastTimestamp = block.timestamp;
     }
 
-    function enterRaffle() public payable {
+    function enterRaffle() external payable {
         if (msg.value < i_entranceFee) revert Raffle__NotEnoughETH();
+
+        s_players.push(payable(msg.sender));
+
+        emit RaffleEnter(msg.sender);
     }
 
-    function pickWinner() public {}
+    // 1. Get random number
+    // 2. Use random number to pick a player
+    // 3. Be automatically called
+    function pickWinner() public {
+        // Check the interval
+        if (block.timestamp - s_lastTimestamp > i_interval) revert Raffle__NotEnoughTimeHasPassed();
+
+        // Get random number
+
+        s_lastTimestamp = block.timestamp;
+    }
 
     /**
      * Getters
