60b7e6902934253542ab7faec8b6a957a612d776
Nikolay Ivanov <nick.ivanov98@gmail.com>
2024-11-23 07:03:19 +0200

    Adding simple Raffle

---
+++ src/Raffle.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+/**
+ * @title Raffle
+ * @author Nikolay P. Ivanov
+ * @notice This contract is for creating a raffle.
+ * @dev Implements Chainlink VRFv2.5
+ */
+contract Raffle {
+    function enterRaffle() public {}
+    function pickWinner() public {}
+}
